e034a99b2ba6fbef0d0f47241c8e0ab85bb0c3ea
Raphael <raphael.thiney@gmail.com>
2016-06-08 18:43:41 -0400

    removed errant code

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,7 @@ namespace CleanShave
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddGlimpse();
-services.
+           services.AddGlimpse(); 
           services.Configure<SiteSettings>(settings =>
             {
 
